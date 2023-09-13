db27e4c731f3d1b816047a2932d32374a158e777
Leo <leo.a.alho@gmail.com>
2023-09-13 23:37:14 +0300

    Fixed problem with different sized maps. Updated README and minimizing the build

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # A minimal web map client
+Picomap is a hyperminimalist map client without any dependencies written in native JS.
 The source code for the project is in the ./src directory
-the build and minified version is ./dist/picomap.js (UMD) and ./dist/picomap-es.js (ES).
 
-## How to use it
+## How to use the map
 Add a div in your project with id="map". In addition add the following script to your project: `<script src="https://unpkg.com/picomap/dist/picomap.js"></script>`
 
-### Example
+### Minimal example
 index.html:
 ```
 <!DOCTYPE html>
@@ -26,8 +26,21 @@ mapInstance.initialize();
 ```
 
 Picomap accepts the following attributes, none of which are required:
-- height: number (pixels)
-- width: number (pixels)
-- latitude: number (-85.06-85.06)
-- longitude: number (-180-180)
-- zoom: number (0-19)
+| Name   | Type | Default Value | Accepted values | Description
+| -------- | ------- | ------ | --- | --- |
+| height  | number   | 500 | | Height of the map |
+| width | number    | 500 | | Width of the map|
+| lat    | number   | 60.5 | -85.06-85.06 | Latitude for the map centre |
+| lon | number | 24.4391 | -180-180| Longitude for the map centre |
+| zoom | number | 10 | 0-19 | Initial zoom level
+|id  | string | "map" | | Id of the map's div|
+| source | string | "https://tile.openstreetmap.org" || Source for the raster tiles|
+
+## Building from source
+1. Clone the git library: `https://github.com/leoalho/picomap.git`
+2. Install dependecies: `npm install`
+3. Build the files: `npm run build`\
+This will build three different files to to ./dist directory.
+
+## Using with npm
+The library is possible to install with npm by running `npm install picomap`. The files for the library are as specified in the las section. Both ES import and CommonJS require syntax should work.
--- src/picomap.js
+++ src/picomap.js
@@ -1,18 +1,19 @@
-import { lon2tile, lat2tile, tileOffset } from "./utils.js";
+import { lon2tile, lat2tile, tileOffset, nTiles, createElement } from "./utils.js";
 
 export default class Picomap {
-  constructor(height = 500, width = 500, lon = 24.4391, lat = 60.5, zoom = 10) {
+  constructor(height = 500, width = 500, lon = 24.4391, lat = 60.5, zoom = 10, id="map", source="https://tile.openstreetmap.org") {
     this.height = height;
     this.width = width;
     this.lat = lat;
     this.lon = lon;
     this.zoom = zoom;
-    this.map = document.getElementById("map");
+    this.source = source
+    this.map = document.getElementById(id);
     this.map.style = `height: ${this.height}px; width: ${this.width}px; overflow: hidden; transform: translate3d(0px,0px,0px)`;
   }
 
   #createButton(text, x,y,z, left, top){
-    let button = document.createElement("button");
+    let button = createElement("button");
     button.innerText = text;
     button.style = `width: 20px; position: absolute; top: ${top}px; left: ${left}px`;
     button.addEventListener("click", () => this.#move(x,y,z));
@@ -20,24 +21,24 @@ export default class Picomap {
   }
 
   #createControlLayer() {
-    let controlLayer = document.createElement("div");
+    let controlLayer = createElement("div");
     controlLayer.style = "height: 100%; width: 100%; position: absolute; top: 0px; left: 0px";
     controlLayer.append(this.#createButton("\u25B2", 0, 1, 0, 40, 20)); //Up
-    controlLayer.append(document.createElement("br"));
+    controlLayer.append(createElement("br"));
     controlLayer.append(this.#createButton("\u25C0", -1, 0, 0, 20, 40)); //Left
     controlLayer.append(this.#createButton("\u25B6", 1, 0, 0, 60, 40)); //Right
-    controlLayer.append(document.createElement("br"));
+    controlLayer.append(createElement("br"));
     controlLayer.append(this.#createButton("\u25BC", 0, -1, 0, 40, 60)); //Down
-    controlLayer.append(document.createElement("br"));
+    controlLayer.append(createElement("br"));
     controlLayer.append(this.#createButton("+", 0, 0, 1, 40, 100));
-    controlLayer.append(document.createElement("br"));
+    controlLayer.append(createElement("br"));
     controlLayer.append(this.#createButton("-", 0, 0, -1, 40, 120));
     this.map.append(controlLayer);
   }
 
   #createTile(x, y, z, transX, transY) {
-    const tile = document.createElement("img");
-    tile.src = `https://tile.openstreetmap.org/${x}/${y}/${z}.png`;
+    const tile = createElement("img");
+    tile.src = `${this.source}/${x}/${y}/${z}.png`;
     tile.alt = "";
     tile.style = `width: 256px; height: 256px; opacity: 1; transform: translate3d(${transX}px, ${transY}px, 0px); display: block; position: absolute`;
     return tile;
@@ -48,21 +49,19 @@ export default class Picomap {
     let centerY = lat2tile(this.lat, this.zoom);
     let offset = tileOffset(this.zoom, this.lon, this.lat);
     this.mapLayer.style.transform = `translate3d(${128 - offset.x}px,${128 - offset.y}px,0px)`;
-    let noTilesY = Math.floor(this.height / 256);
-    let noTilesX = Math.floor(this.width / 256);
     const tiles = [];
-    for (let i = -noTilesY; i < noTilesY + 1; i++) {
-      for (let j = -noTilesX; j < noTilesX + 1; j++) {
-        let transY = ((this.height / 2 - 128) % 256) + i * 256;
-        let transX = ((this.width / 2 - 128) % 256) + j * 256;
-        tiles.push(this.#createTile(this.zoom, centerX + j, centerY + i, transX, transY));
+    for (let i = 0; i < 2*nTiles(this.height) + 1; i++) {
+      for (let j = 0; j < 2* nTiles(this.width) + 1; j++) {
+        let transY = -((this.height / 2 - 128) % 256) + i * 256;
+        let transX = -((this.width / 2 - 128) % 256) + j * 256;
+        tiles.push(this.#createTile(this.zoom, centerX + j - nTiles(this.width), centerY + i - nTiles(this.height), transX, transY));
       }
     }
     this.mapLayer.replaceChildren(...tiles);
   }
 
   #createMapLayer() {
-    this.mapLayer = document.createElement("div");
+    this.mapLayer = createElement("div");
     this.mapLayer.style = "height: 100%; width: 100%";
     this.#renderTiles();
     this.map.append(this.mapLayer);
