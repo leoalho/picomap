ec7505ca29d820fd67ff590561c8e6f4d7074320
leoalho <leo.a.alho@gmail.com>
2023-10-24 07:39:01 +0300

    Changes in utils

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,14 @@
 const R = 6378137;
-const C = 2 * Math.PI * R;
+const PI = Math.PI;
+const C = 2 * PI * R;
 
-export const nTiles = length => Math.ceil((length/2-128)/256);
-export const lon2tile = (lon, zoom) => Math.floor(((lon + 180) / 360) * Math.pow(2, zoom));
-export const lat2tile = (lat, zoom) => Math.floor(((1 -Math.log(Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180)) /Math.PI) /2) *Math.pow(2, zoom));
+export const nTiles = length => Math.ceil((length/2 - 128) / 256);
+export const lon2tile = (lon, zoom) => Math.floor(((lon + 180) / 360) * 2 ** zoom);
+export const lat2tile = (lat, zoom) => Math.floor(((1 -Math.log(Math.tan((lat * PI / 180) / 2 + PI / 4)) /PI) /2) * 2 ** zoom);
 export const createElement = (tag) => document.createElement(tag);
 
 export const tileOffset = (z, lon, lat) => {
-  let pixels = 256 * Math.pow(2, z);
-  let cordy = (C / 2 - R * Math.log(Math.tan((lat * Math.PI / 180) / 2 + Math.PI / 4))) / C;
-  return {x: parseInt(((180 + lon) / 360) * pixels) % 256, y: parseInt(cordy * pixels) % 256};
+  let pixels = 256 * 2**z;
+  let cordy = (C / 2 - R * Math.log(Math.tan((lat * PI / 180) / 2 + PI / 4))) / C;
+  return {x: parseInt(((lon + 180) / 360) * pixels) % 256, y: parseInt(cordy * pixels) % 256};
 }
